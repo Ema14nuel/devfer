61e3b38348cfd03797d9a96e7ad47b0d4b104e89
Ema14nuel <fdsolinf@gmail.com>
2023-09-01 14:13:03 -0500

    configuracion API pokemon

--- js/rick.js
+++ js/rick.js
@@ -1,1 +1,12 @@
 console.log("Esta esta bien");
+
+import fetch from 'fetch';
+
+const url = 'https://pokeapi.co/api/v2/pokemon/';
+const response = fetch(url);
+
+const pokemon = await response.json();
+
+for (const pokemon of pokemon) {
+  console.log(`${pokemon.name} - ${pokemon.type}`);
+}
